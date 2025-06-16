2d4f958d1f4645348ff2cbadbf13cc946fb7874e
sLucaveche <steffano.lucaveche.2002@gmail.com>
2025-06-16 19:55:37 -0400

    Add files via upload

---
+++ WPF/Pages/RegistroCaso.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace registrar_casos
+{
+ 
+    public partial class RegistroCaso : Window
+    {
+        public RegistroCaso()
+        {
+            InitializeComponent();
+        }
+        private void BGuardar_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Guardando el caso");
+        }
+
+        private void BCancelar_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void BAdjuntar_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("...");
+        }
+    }
+}
